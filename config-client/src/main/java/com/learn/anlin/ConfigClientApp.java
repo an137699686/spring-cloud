cedb83f558603dde0ef7c79c586d562f343c777c
Your Name <you@example.com>
2021-09-11 09:52:36 +0800

    an

---
+++ config-client/src/main/java/com/learn/anlin/ConfigClientApp.java
@@ -0,0 +1,31 @@
+package com.learn.anlin;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAutoConfiguration
+@Configurable
+@RestController
+public class ConfigClientApp
+{
+    @Value("${config.name}")
+    int name;
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello " + name;
+    }
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ConfigClientApp.class,args);
+    }
+}
