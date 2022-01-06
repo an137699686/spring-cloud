8bf1641a379988e6e69a1fbc4a10f4f6a5488b08
“‘anlin <137699686@qq.com>
2022-01-06 16:08:50 +0800

    集成gateway,ribbon实现负载均衡

---
+++ gate-way-demo/src/test/java/com/anlin/gatewaydemo/GateWayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anlin.gatewaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
